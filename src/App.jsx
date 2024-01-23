50c3901bde19f2ec5f7667d1513c75c9e82d41f1
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-23 16:12:58 +0300

    Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Navigation from "./components/Navigation/Navigation";
 import Landing from "./components/Landing/Landing";
 import Footer from "./components/Footer/Footer";
 import { createBrowserRouter } from "react-router-dom";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <>
       <header>
-          <Navigation />
-          <Landing />
+        <Navigation />
+        <Landing />
       </header>
       <main>
-        <div className="container"></div>
+        <div className="container">
+          <Projects />
+        </div>
       </main>
       <Footer />
     </>
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,63 @@
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardText,
+  MDBCardTitle,
+} from "mdb-react-ui-kit";
+import React from "react";
+import "./projects.css";
+import word from "../../assets/word.jpg";
+import sunset from "../../assets/sunset.jpg";
+import night from "../../assets/night.jpg";
+const Projects = () => {
+  const PROJECTS = [
+    [
+      "SEKUCU",
+      sunset,
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Aliquid, consequuntur soluta. Facilis, expedita nesciunt temporaliquam fugiat eaque. Cumque, eligendi?",
+    ],
+    [
+      "Uzima",
+      word,
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Aliquid, consequuntur soluta. Facilis, expedita nesciunt temporaliquam fugiat eaque. Cumque, eligendi?",
+    ],
+    [
+      "Fairprice",
+      night,
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Aliquid, consequuntur soluta. Facilis, expedita nesciunt temporaliquam fugiat eaque. Cumque, eligendi?",
+    ],
+  ];
+  return (
+    <div className="container-fluid">
+      <div className="row py-3">
+        {PROJECTS.map(([name, image, text]) => {
+          return (
+            <div className="col-md-6 col-lg-4 bg-tertiary my-5 mb-3">
+              <MDBCard
+                className="bg-transparent"
+                style={{ border: "1px solid white" }}
+              >
+                <MDBCardImage
+                  src={image}
+                  className="mx-auto rounded-6 shadow-4 position-relative project-image"
+                  style={{
+                    width: "80%",
+                    bottom: "1.5rem",
+                    height: "250px",
+                  }}
+                />
+                <MDBCardBody>
+                  <MDBCardTitle>{name}</MDBCardTitle>
+                  <MDBCardText>{text}</MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
