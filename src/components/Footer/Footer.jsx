10557b06639647ae1560a44ec51d3a7c9aca5e66
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-01-23 17:34:34 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
 const Footer = () => {
   return (
     <footer>
-      
+      <div className="d-flex justify-content-center py-3">
+        <i class="fab fa-github"></i>
+        <i class="fab fa-linkedin-in"></i>
+        <i class="far fa-envelope"></i>
+        <i class="fas fa-phone"></i>
+      </div>
+      <div className="d-flex justify-content-center py-3">
+        <span>
+          &copy; 2024 Copyright:&nbsp; <Link to="leetech.top">Leetech.top</Link>
+        </span>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
