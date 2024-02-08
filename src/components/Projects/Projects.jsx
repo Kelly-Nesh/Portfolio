f75c090157daecffc0d94cd3183cf0adf0a9eca7
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-02-08 13:54:01 +0300

    updated projects design and images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,56 +7,56 @@ import {
 } from "mdb-react-ui-kit";
 import React from "react";
 import "./projects.css";
-import word from "../../assets/word.jpg";
-import sunset from "../../assets/sunset.jpg";
-import night from "../../assets/night.jpg";
+import fairprice from "../../assets/fairprice1.png";
+import sekucu from "/src/assets/sekucu.png";
+import { Parallax } from "react-scroll-parallax";
+const uzima =
+  "https://scontent.fnuu1-1.fna.fbcdn.net/v/t39.30808-6/415028714_666671675669945_7466691171484515501_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=zgU2_W9r71wAX9hsKVt&_nc_zt=23&_nc_ht=scontent.fnuu1-1.fna&oh=00_AfB22YU0dtiDX1g_K_evkLM0IyqvSsAci2gkjwwVjSMslw&oe=65CA382D";
 
 const Projects = () => {
   const PROJECTS = [
     [
       "SEKUCU",
-      sunset,
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Aliquid, consequuntur soluta. Facilis, expedita nesciunt temporaliquam fugiat eaque. Cumque, eligendi?",
+      sekucu,
+      "In my term as the chairperson, I designed and developed a website with Django as its backend. I am currently involved in its maintenance and making updates to the website.",
     ],
     [
       "Uzima",
-      word,
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Aliquid, consequuntur soluta. Facilis, expedita nesciunt temporaliquam fugiat eaque. Cumque, eligendi?",
+      uzima,
+      "This project involves developing a digital platform for a local church. The website serves as a hub for the church community, providing information about service times, upcoming events, and church news. Currently in development",
     ],
     [
       "Fairprice",
-      night,
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit.Aliquid, consequuntur soluta. Facilis, expedita nesciunt temporaliquam fugiat eaque. Cumque, eligendi?",
+      fairprice,
+      "Local salon website. This project involved creating an  online presence for a local salon. The website was designed to reflect the salon’s warm and welcoming atmosphere, while providing customers with easy access to service descriptions, products, and clients options.",
     ],
   ];
   return (
-    <div className="container-fluid" id="projects">
-      <div className="row py-3">
-        {PROJECTS.map(([name, image, text]) => {
-          return (
-            <div className="col-md-6 col-lg-4 bg-tertiary my-5 mb-3" key={name}>
-              <MDBCard
-                className="bg-transparent"
-                style={{ border: "1px solid #464b4e" }}
-              >
-                <MDBCardImage
-                  src={image}
-                  className="mx-auto rounded-6 shadow-4 position-relative project-image"
-                  style={{
-                    width: "80%",
-                    bottom: "1.5rem",
-                    height: "250px",
-                  }}
-                />
-                <MDBCardBody>
-                  <MDBCardTitle>{name}</MDBCardTitle>
-                  <MDBCardText>{text}</MDBCardText>
-                </MDBCardBody>
-              </MDBCard>
-            </div>
-          );
-        })}
-      </div>
+    <div className="row pr-row py-3">
+      {PROJECTS.map(([name, image, text]) => {
+        return (
+          <div className="col-sm-6 col-lg-4 my-5 mb-3 project-col" key={name}>
+            <MDBCard
+              className="project-card"
+              style={{ border: "1px solid #464b4e" }}
+            >
+              <MDBCardImage
+                src={image}
+                className="mx-auto rounded-6 position-relative project-image object-cover"
+                style={{
+                  width: "85%",
+                  bottom: "1.5rem",
+                  height: "250px",
+                }}
+              />
+              <MDBCardBody>
+                <MDBCardTitle className="text-center">{name}</MDBCardTitle>
+                <MDBCardText>{text}</MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -65,21 +65,26 @@ export default Projects;
 
 export const Persona = () => {
   return (
-    <div
-      className="mx-auto bg-image"
-      style={{
-        height: "50vh",
-        backgroundImage:
-          "url(https://images.pexels.com/photos/395196/pexels-photo-395196.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      }}
-    >
-      <div className="h-100 mask d-grid" style={{ backgroundColor: "hsla(0, 0%, 0%, 0.5", placeItems: "center" }}>
-        <p className="text-white text-center display-6">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus cum
-          laborum laboriosam eum voluptatum necessitatibus illo, consequatur
-          aperiam eius asperiores!
-        </p>
-      </div>
-    </div>
+    <>
+      <Parallax className="mx-auto bg-image min-vh-50">
+        <div
+          className="h-100 d-flex flex-wrap p-3 gap-2 persona"
+          style={{
+            backgroundColor: "hsla(0, 0%, 0%, 0.5",
+            placeItems: "center",
+          }}
+        >
+          <p className=" h4">
+            Involved in developing and maintaining Django-based websites for
+            diverse clients, ranging from a small business to non-profit
+            organizations.
+          </p>
+          <p className=" h4">
+            I offer my services pro bono as a strategy to enhance my skills and
+            build a substantial portfolio
+          </p>
+        </div>
+      </Parallax>
+    </>
   );
 };
